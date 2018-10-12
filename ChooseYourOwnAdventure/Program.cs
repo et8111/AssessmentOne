7b3dd005df17469f1f7208a086ae076e17c60e65
Jordan Owiesny <jordanowiesny@quickenloans.com>
2018-10-12 15:43:04 -0400

    made text funner

--- ChooseYourOwnAdventure/Program.cs
+++ ChooseYourOwnAdventure/Program.cs
@@ -43,7 +43,7 @@ namespace ChooseYourOwnAdventure
             string c = "";
             while (true)
             {
-                Console.WriteLine("ESCAPE THE ZOMBIE\n");
+                Console.WriteLine("A ZOMBIE IS RIGHT BEHIND YOU AND IT'S TRYING TO FREAKIN' MURDER YOU!\n");
                 Console.WriteLine("Press Any Key to begin . . .");
                 Console.ReadLine();
                 string[] paths = { "Take ladder", "Walk straight", "turn left", "turn right", "Dark hole on floor...", "Crawl through hole in wall" };
